d0c314a22e2fdc3962cc1816f645497b8b0ddb6a
kulsoom ansari <kulsoomansari412@gmail.com>
2020-06-07 06:42:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+                                           //ALERTS
+                                          //QUESTION 1
+var x = "welcome to our website";
+alert(x);
+                                           //QUESTION 2 
+var x= "JAVASCRIPT ALERT\nError!please enter a valid password."
+alert(x);
+                                          //QUESTION 3
+var x= "JAVASCRIPT ALERT\nWelcome to JS land...\nHappy coding."
+alert(x);
+                                         //QUESTION 4
+var x = "JAVASCRIPT ALERT\nHappy coding."
+
+
+
+                                         //QUESTION 5
+var x = "JAVASCRIPT ALERT\nHello...I can run js through my web browser's console";
+console.log(x);
+                                         //CHAPTER 2
+                                         //QUESTION 1
+var x = "username";
+document.write(x);
+document.write("</br>");
+document.write("</br>");
+                                          //QUESTION 2 
+var myName = "Kulsoom ansari";
+document.write(myName);
+document.write("</br>");
+document.write("</br>");
+                                          //QUESTION 3
+var message = "Hello world";
+alert(message);
+                                          //QUESTION 4
+var x = "kulsoom ansari\n20 years old\ncertified mobile application development";
+alert(x);
+                                           //QUESTION 5
+var x = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(x);
+                                            //QUESTION 6
+var email = "kulsoomansari412@gmail.com";
+alert("My email address is " + email);
+                                            //QUESTION 7
+var book = "A smarter way to learn javascript";
+alert("I am trying to learn from the book " + book);
+//QUESTION 4
+var x = "Yah! I can write HTML content through Javascript";
+document.write(x);
+document.write("</br>");
+document.write("</br>");
+                                                          //CHAPTER 3
+                                                         //QUESTION 1
+var age = 21;
+alert("I am " + age + " years old");
+                                                         //QUESTION 2
+ var birthYear = 1998;
+ document.write("My birth year is " + birthYear);
+ document.write("</br>");
+ document.write("</br>");
+                                                         //QUESTION 3
+var visitorName = "John Doe";
+var order = 5;
+var store = "xyz clothing store";
+document.write(visitorName + " ordered " + order + " T-shirt in " + store + ".");
+document.write("</br>");
+document.write("</br>");
+                                                          //CHAPTER 5
+                                                        //QUESTION 1
+var x = 3;
+var y = 5;
+var z = x+y;
+document.write("Sum of " + x + " and " + y + " is " + z);
+document.write("</br>");
+document.write("</br>");
+                                                       //QUESTION 2
+var x = 5;
+var y = 3;
+var z = x-y;
+document.write("Diffrence of " + x + " and " + y + " is " + z);
+document.write("</br>");
+document.write("</br>");
+                      
+ var x = 3;
+var y = 5;
+var z = x/y;
+document.write("Modulus of " + x + " and " + y + " is " + z);
+document.write("</br>");
+ document.write("</br>");
+                      
+ var x = 3;
+var y = 5;
+var z = x*y;
+document.write("Multiplication of " + x + " and " + y + " is " + z);
+document.write("</br>");
+ document.write("</br>");
+                                                    //QUESTION 3
+
+var i = 5;
+document.write("value of i is " + i);
+document.write("</br>");
+ document.write("</br>");
+for(var i = 5; i <= 6; i++){
+  document.write("initial value is " + "" + "<\br>")
+};
